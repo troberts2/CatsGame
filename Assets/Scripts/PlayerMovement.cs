bf04730d1d652ea80b521ffc07286a5943f6ead0
troberts2 <troberts2@mail.bradley.edu>
2023-10-17 16:34:03 -0500

    initial code commented and adjusted

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -26,17 +27,21 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void OnEnable(){
+        //sets up the playercontrols
         move = playerControls.Player.Move;
         move.Enable();
+
         jump = playerControls.Player.Jump;
         jump.Enable();
         jump.performed += Jump;
+
         dash = playerControls.Player.Dash;
         dash.Enable();
         dash.performed += Dash;
     }
 
     private void OnDisable(){
+        //disables player controls
         move.Disable();
         jump.Disable();
         dash.Disable();
@@ -47,25 +52,41 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDir = move.ReadValue<Vector2>();
     }
-
+    /// <summary>
+    /// currently just moves the player
+    /// </summary>
     private void FixedUpdate() {
+        //basic movement with addforce
         rb.AddForce(new Vector2(moveDir.x * moveSpeed, 0), ForceMode2D.Force);
     }
+    /// <summary>
+    /// jump functionallity. Jumps as many times as want just change max jump count to as many jumps
+    /// they should be aloud.
+    /// </summary>
+    /// <param name="context"></param>
     private void Jump(InputAction.CallbackContext context){
         if(jumpCount > 0){
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             jumpCount--;
         }
     }
-
+    /// <summary>
+    /// dash functionallity. Adds force as long as player is moving in a direction
+    /// </summary>
+    /// <param name="context"></param>
     private void Dash(InputAction.CallbackContext context){
-        if(dashCount > 0){
+        if(dashCount > 0 && moveDir != Vector2.zero){
             Debug.Log("dash");
             rb.AddForce(moveDir * dashForce, ForceMode2D.Impulse);
             dashCount--;
             StartCoroutine(DashTimer());
+            StartCoroutine(DashConstraint());
         }
     }
+    /// <summary>
+    /// resets the jump count
+    /// </summary>
+    /// <param name="other"></param>
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.collider.tag == "ground"){ 
             isGrounded = true;
@@ -75,10 +96,24 @@ public class PlayerMovement : MonoBehaviour
     private void OnCollisionExit2D(Collision2D other) {
         if(other.collider.tag == "ground"){ isGrounded = false;}
     }
+    /// <summary>
+    /// dash timer and count
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator DashTimer(){
         yield return new WaitForSeconds(2f);
         if(dashCount < dashCountMax){
             dashCount++;
         }
+    }
+    /// <summary>
+    /// sets gravity to 0 while dashing intially to let player have a more floaty dash
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DashConstraint(){
+        float tempGravScale = rb.gravityScale;
+        rb.gravityScale = 0;
+        yield return new WaitForSeconds(.3f);
+        rb.gravityScale = tempGravScale;
     }
 }
